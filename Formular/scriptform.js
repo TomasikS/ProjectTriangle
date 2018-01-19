4bb6039b320058604f8fd578af4514f3d00c1be5
studentsovy <stefan.tomasik@akademiasovy.sk>
2018-01-19 12:21:19 +0100

    update

--- Formular/scriptform.js
+++ Formular/scriptform.js
@@ -1,4 +1,4 @@
-(function($) {
+$(document).ready(function() {
 
 var persons = new  Array();
 
@@ -23,77 +23,120 @@ var persons = new  Array();
 		}	
 	});
 
-	$("#fName").keyup(function(){
-		var fName = $("#fName").val();
-		if(fName.trim().length()>1){
-			$("#errfname").html("");
-		}
-	});
-	$("#lName").keyup(function(){
-		var lName = $("#lName").val();
-		if(lName.trim().length()>1){
-			$("#errlname").html("");
-		}
-	});
-	
-	var width = $(".getSizeFromInput").css("width");
-	$(".setSize").css({"width" : wdth});
+	$("#fname").keyup(function(event) {
+     var fname=$("#fName").val();
+     if(fname.trim().length>1){
+       $("#errfname).html("");
+     }
+     /* Act on the event */
+   });
 
- 
+   $("#lname").keyup(function(event) {
+     var lname=$("#lname").val();
+     if(lname.trim().length>1){
+       $("#errlname").html("");
+     }
+     /* Act on the event */
+   });
 
- function ProcessInputData(){
-                var person = new Object();
-                person.fname = $("#fName").val();
-                person.lname = $("#lName").val();
-                person.date = $("#compareDate").val();
-                person.gender = $("input[name='gender']:checked").val();
-                console.log(person);
-                persons.push(person);
-                printTable();
-            };
-            ProcessInputData();
+   function processFormData(){
+     var newPerson=new Object();
+     newPerson.firstName=$("#fName").val();
+     newPerson.lastName=$("#lName").val();
+     newPerson.dob=$("#birthdate").val();
+     newPerson.gender=$("input[name='gender']:checked").val();
 
-function printTable(){
-                if(persons.length>0){
-                    var divTable=$('.table');
-                    divTable.empty();
-                    var table=$("<table/>");
-                    var line=$("<tr/>");
-                    var col1=$("<th/>");
-                    $(col1).append('First name');
-                    var col2=$("<th/>");
-                    $(col2).append('Last name');
-                    var col3=$("<th/>");
-                    if(CheckEdge)$(col3).append('Age');
-					else $(col3).append('Date of birth');
-					
-                    var col4=$("<th/>");
-                    $(col4).append('Gender');
-                    var col5=$("<th/>");
-                    $(col5).append('Delete');
-                    line.append(col1);
-                    line.append(col2);
-                    line.append(col3);
-                    line.append(col4);
-                    line.append(col5);
-                    table.append(line);
-                    divTable.append(table);
-                }
+     persons.push(newPerson);
+     printTable();
 
+   }
 
-})
+   function printTable(){
+     var tablediv=$('.table');
+     tablediv.empty();
+     var selectedGender = getSelectedGender();
+     var checkedAge=isCheckedAge();
 
+     var arr=new Array();
 
-(function IsCheckAge(){
-if ($"showage")	
-	
-})
+     persons.forEach(function(obj){
 
+       if(selectedGender == 'm' && obj.gender=='true'){
+         //console.log("muz");
+            arr.push(obj);
+       }
+       if(selectedGender == 'f' && obj.gender=='false'){
+         //console.log("zeny");
+            arr.push(obj);
+       }
+       if(selectedGender == 'a'){
+         //console.log("muzy a zeny");
+            arr.push(obj);
+       }
+     });
 
+     if(arr.length>0){
 
+        var table = $("<table/>");
+        var line=$("<tr/>");
+        var col1=$("<th/>");
+        $(col1).append('First name');
+        var col2=$("<th/>");
+        $(col2).append('Last name');
+        var col3=$("<th/>");
+        if(checkedAge){
+              $(col3).append('Age');
+        }
+        else {
+             $(col3).append('Date of birth');
+        }
+        var col4=$("<th/>");
+        $(col4).append('Gender');
+        var col5=$("<th/>");
+        $(col5).append('Delete');
+        line.append(col1);
+        line.append(col2);
+        line.append(col3);
+        line.append(col4);
+        line.append(col5);
+        table.append(line);
+        tablediv.append(table);
+     }
+     //for(i=0;i<persons.size();i++){}
+     arr.forEach(function(obj){
+        var line=$("<tr/>");
+        var col1=$("<td/>");
+        $(col1).append(obj.firstName);
+        var col2=$("<td/>");
+        $(col2).append(obj.lastName);
+        var col3=$("<td/>");
+        $(col3).append(obj.dob);
+        var col4=$("<td/>");
+        $(col4).append(obj.gender);
+        line.append(col1);
+        line.append(col2);
+        line.append(col3);
+        line.append(col4);
+        table.append(line);
+     });
 
+   }
 
-(jQuery);
+   function getSelectedGender(){
+      var value = $("#selectGender").val();
+      return value;
+   }
 
+   $("#selectGender").change(function(event) {
+     /* Act on the event */
+     printTable();
+   });
 
+   function isCheckedAge(){
+     return $("#showAge").is(':checked');
+   }
 
+   $("#showAge").change(function(event){
+     printTable();
+   });
+});
